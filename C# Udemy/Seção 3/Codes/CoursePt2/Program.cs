f4d329328be424222f43ca9079f287c152506383
Josue <geenujunior@gmail.com>
2026-01-17 04:43:36 -0300

    lógica de programação com c# funções, laços e condicionais

---
+++ C# Udemy/Seção 3/Codes/CoursePt2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CoursePt2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// Operadores Comparativos
+
+			int a = 10;
+			bool c1 = a < 10;
+			bool c2 = a < 20;
+			bool c3 = a > 10;
+			bool c4 = a > 5;
+
+            Console.WriteLine(c1);
+			Console.WriteLine(c2);
+			Console.WriteLine(c3);
+			Console.WriteLine(c4);
+
+			// Operadores lógicos
+			Console.WriteLine(3 > 5 && 2 == 1);
+
+
+
+			// Estruturas Condicionais
+
+			int x = 10;
+
+			Console.WriteLine("Bom Dia");
+
+			if (x < 5){
+				Console.WriteLine("Boa Tarde");
+			}
+
+			Console.WriteLine("Boa Noite");
+
+
+			int z = int.Parse(Console.ReadLine());
+
+			if (z % 2 == 0)
+			{
+				Console.WriteLine("É par!");
+			}
+			else
+			{
+				Console.WriteLine("É ímpar!");
+			}
+
+            Console.WriteLine("Que horas são?");
+			int h = int.Parse(Console.ReadLine());
+
+			if (h < 12)
+			{
+				Console.WriteLine("Bom dia!");
+			} else if (h < 18 && h >= 12)
+			{
+				Console.WriteLine("Boa Tarde!");
+			}
+			else
+			{
+				Console.WriteLine("Boa Noite!");
+			}
+
+
+
+		}
+
+	}
+}
---
+++ C# Udemy/Seção 3/Codes/CoursePt3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CoursePt3
+{
+	internal class Program
+	{
+
+		static void Main(string[] args)
+		{
+
+
+			//Console.Write("Digite um número: ");
+			//double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			//while (x >= 0)
+			//{
+			//double raiz = Math.Sqrt(x);
+			//Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+			//Console.Write("Digite um número: ");
+			//x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			//}
+
+			//Console.WriteLine("Número negativo");
+
+			Console.Write("Quantos números inteiros você vai digitar? ");
+			int n = int.Parse(Console.ReadLine());
+
+			int sum = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+                Console.Write($"Valor #{i}: ");
+				int value = int.Parse(Console.ReadLine());
+				sum += value;
+			}
+
+			Console.WriteLine($"Soma = {sum}");
+
+			
+
+		}
+
+		static int Maior(int a, int b, int c)
+		{
+			int m;
+
+			if (a > b && a > c)
+			{
+				m = a;
+			}
+			else if (b > c)
+			{
+				m = b;
+			}
+			else
+			{
+				m = c;
+			}
+			return m;
+		}
+
+
+
+	}
+}
